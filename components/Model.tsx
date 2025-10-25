583e45c9ab52627efa70a403f19d68ebfc09ad28
NirajSalunke <154681227+NirajSalunke@users.noreply.github.com>
2025-10-26 01:23:06 +0530

    feat: add Model component and integrate Spline for 3D rendering; update styles and dependencies

---
+++ components/Model.tsx
@@ -0,0 +1,9 @@
+import Spline from "@splinetool/react-spline/next";
+
+export default function Model() {
+  return (
+    <main className="w-full h-[113%]">
+      <Spline scene="https://prod.spline.design/UetxeVPx2IeBELie/scene.splinecode" />
+    </main>
+  );
+}
